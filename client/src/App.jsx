548933951bf8181d5195e7fe17c8db12634b82d1
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-03-31 11:28:04 -0300

    limpando configurações iniciais e criando rotas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [backEndData, setBackEndData] = useState([]);
+
+	useEffect(() => {
+		fetch('/usuarios')
+			.then((response) => response.json())
+			.then((data) => {
+				setBackEndData(data);
+				console.log(data);
+			});
+	}, []);
 
 	return (
-		<div className='App'>
-			<div>
-				<a
-					href='https://vitejs.dev'
-					target='_blank'
-				>
-					<img
-						src={viteLogo}
-						className='logo'
-						alt='Vite logo'
-					/>
-				</a>
-				<a
-					href='https://reactjs.org'
-					target='_blank'
-				>
-					<img
-						src={reactLogo}
-						className='logo react'
-						alt='React logo'
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}!</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		<div>
+			<h1>Olá</h1>
+			{typeof backEndData === 'undefined' ? (
+				<p>Loading...</p>
+			) : (
+				backEndData.map((user, index) => {
+					<p key={index}>{user.nome}</p>;
+				})
+			)}
+			{backEndData.map((user, index) => {
+				<p key={index}>{user.nome}</p>;
+			})}
 		</div>
 	);
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRoute, RouterProvider, Route } from 'react-router-dom';
+
+// Páginas
+import App from './App';
+
+// Estilos
+import './index.css';
+
+const router = createBrowserRoute([
+	{
+		element: <App />,
+		children: [
+			// {
+			// 	path: '/',
+			// 	element: <BoasVindas />,
+			// },
+			{
+				path: '/entrar',
+				element: <Entrar />,
+			},
+			{
+				path: '/registro',
+			},
+			{
+				path: '/sair',
+			},
+			{
+				path: '/usuarios',
+			},
+			{
+				path: '/usuarios/:id',
+			},
+			{
+				path: '/auth/usuarios',
+			},
+			{
+				path: '/pets',
+			},
+			{
+				path: '/pets/:id',
+			},
+			{
+				path: '/auth/pets',
+			},
+			{
+				path: '/auth/pets/:id',
+			},
+			{
+				path: '/anuncios',
+			},
+			{
+				path: '/anuncios/:id',
+			},
+			{
+				path: '/auth/anuncios',
+			},
+			{
+				path: '/auth/anuncios/:id',
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
